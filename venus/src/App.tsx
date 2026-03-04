e2630bc43f0e692731ebe3dce717024053892fc2
syswraith <syswraith@proton.me>
2026-03-04 20:23:07 +0530

    frontend :thumbs-up:

--- venus/src/App.tsx
+++ venus/src/App.tsx
@@ -1,9 +1,7 @@
+import { CreateLink } from "@/components/CreateLink";
+
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+    return <CreateLink/>;
 }
 
-export default App
+export default App;
