0c753ce802b8a4997bdd3ed01823b96686f93be2
syswraith <syswraith@proton.me>
2026-03-06 21:39:23 +0530

    react router + routes
    - react router handles routes
    - routes defined for features planned

--- venus/src/App.tsx
+++ venus/src/App.tsx
@@ -1,7 +1,25 @@
-import { CreateLink } from "@/components/CreateLink";
+import { CreateLink,  } from "@/components/CreateLink";
+import { Dashboard  } from "@/components/Dashboard";
+import { Analytics } from "@/components/Analytics";
+import { Login } from "@/components/Login";
+import { Register } from "@/components/Register";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <CreateLink />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/create'     element={ <CreateLink />  } />
+          <Route path='/dashboard'  element={ <Dashboard />   } />
+          <Route path='/analytics'  element={ <Analytics />   } />
+          <Route path='/login'      element={ <Login />       } />
+          <Route path='/register'   element={ <Register />    } />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+
 }
 
 export default App;
